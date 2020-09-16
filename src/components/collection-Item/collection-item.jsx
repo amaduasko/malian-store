4ce6ddba04486821e53665bc0e486fd6431a96a3
Amadu Askofare <amadouaskofare@gmail.com>
2020-09-16 10:52:23 +0400

    add to card button added to cart-item

--- src/components/collection-Item/collection-item.jsx
+++ src/components/collection-Item/collection-item.jsx
@@ -6,49 +6,74 @@ import Card from '@material-ui/core/Card'
 import CardActionArea from '@material-ui/core/CardActionArea'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent'
-import CardActions from '@material-ui/core/CardActions'
+import Button from '@material-ui/core/Button'
 import './collection-item.scss'
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    height: 500,
-  },
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 446,
-  },
-  name: {
-    fontWeight: 500,
-  },
-  price: {},
+    paper: {
+        height: 500,
+        position: 'relative',
+    },
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 446,
+    },
+    name: {
+        fontWeight: 500,
+    },
+    price: {},
+    addToCardBtn: {
+        display: 'none',
+        width: '80%',
+        backgroundColor: '#fff',
+        color: '#000',
+        fontWeight: 600,
+        letterSpacing: 1.2,
+        opacity: 0.7,
+        position: 'absolute',
+        top: 390,
+        right: 0,
+        left: 0,
+        margin: 'auto',
+        '&:hover': {
+            backgroundColor: '#000',
+            color: '#fff',
+        },
+    },
 }))
 
 const CollectionItem = ({ imageUrl, name, price }) => {
-  const classes = useStyles()
+    const classes = useStyles()
 
-  return (
-    <Grid item md sm={6} xs={12}>
-      <Paper className={classes.paper} elevation={3}>
-        <Card className={classes.root}>
-          <CardActionArea>
-            <CardMedia
-              className={classes.media}
-              image={`${imageUrl}`}
-              title={`${name}`}
-            />
-            <CardContent>
-              <div className='collection-footer'>
-                <span className={classes.name}>{name}</span>
-                <span className={classes.price}>{price}</span>
-              </div>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </Paper>
-    </Grid>
-  )
+    return (
+        <Grid item md sm={6} xs={12}>
+            <Paper className={classes.paper + ' card-paper'} elevation={3}>
+                <Card className={classes.root}>
+                    <CardActionArea>
+                        <CardMedia
+                            className={classes.media + ' media'}
+                            image={`${imageUrl}`}
+                            title={`${name}`}
+                        />
+                        <CardContent>
+                            <div className='collection-footer'>
+                                <span className={classes.name}>{name}</span>
+                                <span className={classes.price}>${price}</span>
+                            </div>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+                <Button
+                    className={classes.addToCardBtn + ' add-card-btn'}
+                    variant='contained'
+                >
+                    Add to card
+                </Button>
+            </Paper>
+        </Grid>
+    )
 }
 
 export default CollectionItem
--- src/pages/Shop/Shop.page.jsx
+++ src/pages/Shop/Shop.page.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Container from '@material-ui/core/Container'
 import CollectionPreviewItem from '../../containers/collection-preview/collection-preview'
 import { SHOP_DATA } from '../../constants/shopData'
 const ShopPage = () => (
-    <container>
+    <Container>
         {SHOP_DATA.map(({ title, items, id }) => (
             <CollectionPreviewItem key={id} title={title} items={items} />
         ))}
-    </container>
+    </Container>
 )
 
 export default ShopPage
