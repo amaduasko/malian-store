af094509763927b4b425e0d62d3b7ece5428903c
Amadu Askofare <amadouaskofare@gmail.com>
2020-09-24 10:56:20 +0400

    integrate redux-persist to persist user session state, redesign checkout page

--- src/components/checkout-item/CheckoutItem.jsx
+++ src/components/checkout-item/CheckoutItem.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { makeStyles } from '@material-ui/core/styles'
+import TableCell from '@material-ui/core/TableCell'
+import TableRow from '@material-ui/core/TableRow'
+import IconButton from '@material-ui/core/IconButton'
+import ChevronRightRoundedIcon from '@material-ui/icons/ChevronRightRounded'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import ClearIcon from '@material-ui/icons/Clear'
 
 import {
     clearItemFromCart,
@@ -8,29 +15,58 @@ import {
 } from '../../redux/cart/cart.actions'
 import './checkout-item.style.scss'
 
-const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+const useStyles = makeStyles({
+    value: {
+        fontSize: 18,
+    },
+    description: {
+        fontSize: 18,
+        letterSpacing: 1.2,
+    },
+})
+
+const CheckoutItemRow = ({ cartItem, clearItem, addItem, removeItem }) => {
+    const classes = useStyles()
     const { name, imageUrl, price, quantity } = cartItem
     return (
-        <div className='checkout-item'>
-            <div className='image-container'>
-                <img alt='item' src={imageUrl} />
-            </div>
-            <span className='name'>{name}</span>
-            <span className='quantity'>
-                <div className='arrow' onClick={() => removeItem(cartItem)}>
-                    {' '}
-                    &#10094;
+        <TableRow>
+            <TableCell>
+                <div className='image-container'>
+                    <img alt='item' src={imageUrl} />
                 </div>
-                <span className='value'>{quantity}</span>
-                <div className='arrow' onClick={() => addItem(cartItem)}>
-                    &#10095;
+            </TableCell>
+            <TableCell>
+                <span className={classes.description}>{name}</span>
+            </TableCell>
+            <TableCell>
+                <div>
+                    <IconButton
+                        color='primary'
+                        onClick={() => removeItem(cartItem)}
+                    >
+                        <ChevronLeftIcon />
+                    </IconButton>
+                    <span className={classes.value}>{quantity}</span>
+                    <IconButton
+                        color='primary'
+                        onClick={() => addItem(cartItem)}
+                    >
+                        <ChevronRightRoundedIcon />
+                    </IconButton>
                 </div>
-            </span>
-            <span className='price'>{price}</span>
-            <div className='remove-button' onClick={() => clearItem(cartItem)}>
-                &#10005;
-            </div>
-        </div>
+            </TableCell>
+            <TableCell>
+                <span className={classes.value}>{price}</span>
+            </TableCell>
+            <TableCell>
+                <IconButton
+                    color='secondary'
+                    onClick={() => clearItem(cartItem)}
+                >
+                    <ClearIcon />
+                </IconButton>
+            </TableCell>
+        </TableRow>
     )
 }
 
@@ -40,4 +76,4 @@ const mapDispatchToProps = (dispatch) => ({
     removeItem: (item) => dispatch(removeItem(item)),
 })
 
-export default connect(null, mapDispatchToProps)(CheckoutItem)
+export default connect(null, mapDispatchToProps)(CheckoutItemRow)
--- src/pages/Checkout/Checkout.page.jsx
+++ src/pages/Checkout/Checkout.page.jsx
@@ -5,37 +5,75 @@ import {
     selectCartItems,
     selectCartTotal,
 } from '../../redux/cart/cart.selector'
-import CheckoutItem from '../../components/checkout-item/CheckoutItem'
+import CheckoutItemRow from '../../components/checkout-item/CheckoutItem'
+import { makeStyles } from '@material-ui/core/styles'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
 import './checkout.style.scss'
 
-const CheckoutPage = ({ cartItems, total }) => {
-    return (
-        <div className='checkout-page'>
-            <div className='checkout-header'>
-                <div className='header-block'>
-                    <span>Product</span>
-                </div>
-                <div className='header-block'>
-                    <span>Description</span>
-                </div>
-                <div className='header-block'>
-                    <span>Quantity</span>
-                </div>
-                <div className='header-block'>
-                    <span>Price</span>
-                </div>
-                <div className='header-block'>
-                    <span>Remove</span>
-                </div>
-            </div>
-            {cartItems.map((cartItem) => (
-                <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-            ))}
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+    tableHead: {
+        textTransform: 'capitalize',
+        fontWeight: 600,
+        letterSpacing: 1.2,
+    },
+})
 
-            <div className='total'>
-                <span>TOTAL : ${total}</span>
-            </div>
-        </div>
+const CheckoutPage = ({ cartItems, total }) => {
+    const classes = useStyles()
+    return (
+        <>
+            <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label='checkout table'>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell className={classes.tableHead}>
+                                Product
+                            </TableCell>
+                            <TableCell className={classes.tableHead}>
+                                Description
+                            </TableCell>
+                            <TableCell className={classes.tableHead}>
+                                Quantity&nbsp;
+                            </TableCell>
+                            <TableCell className={classes.tableHead}>
+                                Price&nbsp;
+                            </TableCell>
+                            <TableCell></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {cartItems.map((cartItem) => (
+                            <CheckoutItemRow
+                                key={cartItem.id}
+                                cartItem={cartItem}
+                            />
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <Typography
+                variant='h6'
+                gutterBottom
+                style={{
+                    width: 130,
+                    marginLeft: 'auto',
+                    marginTop: '0.5rem',
+                    fontFamily: "'PT Sans', sans-serif",
+                }}
+            >
+                TOTAL : ${total}
+            </Typography>
+        </>
     )
 }
 
