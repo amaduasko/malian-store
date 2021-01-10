80659cad4f9f1d474c4b0a8b529f0d08c9474edb
Amadu ASko <amadouaskofare@gmail.com>
2021-01-10 17:36:44 +0400

    first build test

---
+++ build/precache-manifest.4c3e485ff2a8f10a42d8c56f59eede47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1832436490e8b225489d5e92be3c3798",
+    "url": "/index.html"
+  },
+  {
+    "revision": "601bc1ad0c19ce023cb5",
+    "url": "/static/css/main.5bcfd977.chunk.css"
+  },
+  {
+    "revision": "485b8154cb89fdefba04",
+    "url": "/static/js/2.218c086c.chunk.js"
+  },
+  {
+    "revision": "26f1701ad5e9b9d1c4f591247bc37ded",
+    "url": "/static/js/2.218c086c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "601bc1ad0c19ce023cb5",
+    "url": "/static/js/main.91533392.chunk.js"
+  },
+  {
+    "revision": "3114314f56161feaa567",
+    "url": "/static/js/runtime-main.71827596.js"
+  },
+  {
+    "revision": "e934565919b8092b742dc16947ed9b12",
+    "url": "/static/media/shopping-bag.e9345659.svg"
+  },
+  {
+    "revision": "dafed832ceb036e203983b2d5f9217a7",
+    "url": "/static/media/timbuktu.dafed832.svg"
+  }
+]);
