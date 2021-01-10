642b85212fd89853daf4a0e97d85aecfb4f56f42
Amadu ASko <amadouaskofare@gmail.com>
2021-01-10 17:31:45 +0400

    config file made visible

---
+++ src/config/index.js
@@ -0,0 +1,13 @@
+export const api_info = {
+    API_KEY: 'AIzaSyC0Hxl-iDF9JP6QDvwicgXVXit8X3OKjVU',
+    AUTH_DOMAIN: 'blown-db.firebaseapp.com',
+    DATABASE_URL: 'https://blown-db.firebaseio.com',
+    PROJECT_ID: 'blown-db',
+    STORAGE_BUCKET: 'blown-db.appspot.com',
+    MESSAGING_SENDER_ID: '830720699325',
+    APP_ID: '1:830720699325:web:a2c3b234812a56e0c580d4',
+    MEASUREMENT_ID: 'G-SF93YY1CX3',
+}
+
+export const stripe_public_api_key =
+    'pk_test_51HWbnWIh8xOqFiTOt1eVdAPHpFkEjvS8zv6OHRMMeWcVgEgboArQ7FrYKbwOhXRvLaVT8xpOIKUFvnMeKJ4IdsrC00159lugUV'
